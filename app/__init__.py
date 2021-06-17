2a66f0f2b878eb00e9a79bd769da88ab7f25f2f1
sgibson19 <hogwartzx2020@gmail>
2021-06-16 23:44:17 -0400

    made little changes

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Ji-Oh just change the home.html to index.html to see your index.html
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
