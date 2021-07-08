eb61648ab39af39a2f783161db664af148ba4349
sgibson19 <hogwartzx2020@gmail>
2021-07-08 17:54:02 -0400

    testing github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 flask db migrate
 flask db upgrade
-<<<<<<< HEAD
 gunicorn wsgi:app -w 1 -b 0.0.0.0:80 --capture-output --log-level debug
-=======
-gunicorn wsgi:app -w 1 -b 0.0.0.0:80 --capture-output --log-level debug
->>>>>>> 39dbf4f0f222109211b3ac7da5f9d09bb5c3ccff
